cb7e7404c641def65c93d4a4b941f2ad86e167df
WilliamRelken <relkenwilliam@gmail.com>
2021-04-02 12:48:12 -0400

    converted header to _Layout

--- WFBOOM/Controllers/HomeController.cs
+++ WFBOOM/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace WFBOOM.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
